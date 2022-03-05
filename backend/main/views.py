73281b4f1517fbc70093331be13f112e13e77319
michalprzyl <michal.przyl@gmail.com>
2022-03-06 00:43:34 +0100

    git

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics, mixins
 
-# Create your views here.
+from main.models import Book
+from main.serializers import BookSerializer
+from rest_framework.response import Response
+
+class BookGenericAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    queryset = Book.objects
+    serializer_class = BookSerializer
+
+
+    def get(self, request):
+        print("to jest get BookGenericAPI")
+        return Response({
+            'data': self.list(request).data
+        })
--- frontend/src/header/header.tsx
+++ frontend/src/header/header.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './header.css'
-
+import axios from 'axios';
 const Header = () => {
     const openAddModal = () =>{
         console.log("no i wcisnąłeś")
     }
+
+    const getData = async () =>{
+        await axios.get('http://127.0.0.1:8000/book')
+    }
+
+
+    useEffect(()=>{
+        getData();
+    })
+
     return (
         <div className='header'>
             <div className='header-title'>e-BIBLIOTEKA</div>
